c4433e551192192396c3590bca126533153efe8d
Adam Yaj <hwvxyeej@gmail.com>
2025-03-26 11:34:49 -0500

    Add get-trips, get-shapes, auto truncate

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ import JSZip from 'jszip'
 interface Env {
 	SUPABASE_URL: string;
 	SUPABASE_ANON_KEY: string;
+	EMAIL: string;
+	PASSWORD: string;
 }
 
 export default {
@@ -24,14 +26,14 @@ export default {
 	},
 	async fetch(request, env, ctx): Promise<Response> {
 		const { pathname, searchParams } = new URL(request.url);
-		
+
 		const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
 
 		if (pathname.startsWith('/api/')) {
 			if (pathname === '/api/database-refresh' && request.method === 'POST') {
 				const { data, error } = await supabase.auth.signInWithPassword({
-					email: 'hwvxyeej@gmail.com',
-					password: 'Kamenrider',
+					email: env.EMAIL,
+					password: env.PASSWORD,
 				})
 		
 				if (error) {
@@ -42,6 +44,12 @@ export default {
 
 				return await handlePostDatabaseRefresh(supabase);
 			}
+			else if (pathname === '/api/get-trips' && request.method === 'GET') {
+				return handleGetTrips(supabase, searchParams);
+			}
+			else if (pathname === '/api/get-shapes' && request.method === 'GET') {
+				return handleGetShapes(supabase, searchParams);
+			}
 			else {
 				return new Response('Not Found!', { status: 404 });
 			}
@@ -51,7 +59,15 @@ export default {
 	},
 } satisfies ExportedHandler<Env>;
 
-const dataPacketSize = 1000;
+async function truncateTable(supabase : SupabaseClient<any, "public", any>, tableName : string) {
+	const { error } = await supabase.rpc('truncate_table', { table_name: tableName });
+  
+	if (error) {
+	  console.error(`Error truncating table ${tableName}:`, error);
+	} else {
+	  console.log(`Table ${tableName} truncated successfully.`);
+	}
+}
 
 async function uploadProcessedData(supabase : SupabaseClient<any, "public", any>, processed : {}[] | undefined, bulkSize : number, table : string) {
 	while (processed && processed?.length > 0) {
@@ -69,6 +85,8 @@ async function uploadProcessedData(supabase : SupabaseClient<any, "public", any>
 }
 
 async function handlePostDatabaseRefresh(supabase : SupabaseClient<any, "public", any>): Promise<Response> {
+	const dataPacketSize = 1000;
+
 	const zip = await fetch("https://svc.metrotransit.org/mtgtfs/gtfs.zip")
 	.then(response => response.arrayBuffer()
 	.then(arrayBuffer => JSZip.loadAsync(arrayBuffer)
@@ -80,29 +98,85 @@ async function handlePostDatabaseRefresh(supabase : SupabaseClient<any, "public"
 
 	let fileContents, processed;
 
-	console.log("Uploading shapes");
-	fileContents = await shapesFile?.async("string");
-	processed = fileContents?.split(/\r\n/).filter(n=>n).slice(1).map(line => {
-		const separated = line.split(/,/);
-		return {shape_id: separated[0], shape_pt_lat: separated[1], shape_pt_lon: separated[2], shape_pt_sequence: separated[3], shape_dist_traveled: separated[4]}
-	})
-	await uploadProcessedData(supabase, processed, 1000, "shapes");
-
 	console.log("Uploading routes")
+	truncateTable(supabase, 'routes');
 	fileContents = await routesFile?.async("string");
 	processed = fileContents?.split(/\r\n/).filter(n=>n).slice(1).map(line => {
 		const separated = line.split(/,/);
 		return {route_id: separated[0], route_short_name: separated[2], route_long_name: separated[3], route_color: separated[7], route_sort_order: separated[9]}
 	})
-	await uploadProcessedData(supabase, processed, 1000, "routes");
+	await uploadProcessedData(supabase, processed, dataPacketSize, "routes");
 
 	console.log("Uploading trips")
+	truncateTable(supabase, 'trips');
 	fileContents = await tripsFile?.async("string");
 	processed = fileContents?.split(/\r\n/).filter(n=>n).slice(1).map(line => {
 		const separated = line.split(/,/);
 		return {trip_id: separated[2], route_id: separated[0], shape_id: separated[7], direction_id: separated[4]}
 	})
-	await uploadProcessedData(supabase, processed, 1000, "trips");
+	await uploadProcessedData(supabase, processed, dataPacketSize, "trips");
+
+	console.log("Uploading shapes");
+	truncateTable(supabase, 'shapes');
+	fileContents = await shapesFile?.async("string");
+	processed = fileContents?.split(/\r\n/).filter(n=>n).slice(1).map(line => {
+		const separated = line.split(/,/);
+		return {shape_id: separated[0], shape_pt_lat: separated[1], shape_pt_lon: separated[2], shape_pt_sequence: separated[3], shape_dist_traveled: separated[4]}
+	})
+	await uploadProcessedData(supabase, processed, dataPacketSize, "shapes");
 	
 	return new Response("Successful Refresh!");
+}
+
+async function handleGetTrips(supabase : SupabaseClient<any, "public", any>, searchParams : URLSearchParams): Promise<Response> {
+	const route_id = searchParams.get("route_id");
+	if (route_id === null) {
+		return new Response(JSON.stringify({ error: "missing route_id" }), {
+			status: 400,
+			headers: { 'Content-Type': 'application/json' },
+		});
+	}
+	
+	const { data, error } = await supabase
+	.from('trips')
+	.select('*')
+	.eq('route_id', route_id);
+
+	if (error) {
+		return new Response(JSON.stringify({ error: error.message }), {
+			status: 500,
+			headers: { 'Content-Type': 'application/json' },
+		});
+	}
+
+	return new Response(JSON.stringify(data), {
+		headers: { 'Content-Type': 'application/json' },
+	});
+}
+
+
+async function handleGetShapes(supabase : SupabaseClient<any, "public", any>, searchParams : URLSearchParams): Promise<Response> {
+	const shape_id = searchParams.get("shape_id");
+	if (shape_id === null) {
+		return new Response(JSON.stringify({ error: "missing shape_id" }), {
+			status: 400,
+			headers: { 'Content-Type': 'application/json' },
+		});
+	}
+	
+	const { data, error } = await supabase
+	.from('shapes')
+	.select('*')
+	.eq('shape_id', shape_id);
+
+	if (error) {
+		return new Response(JSON.stringify({ error: error.message }), {
+		status: 500,
+		headers: { 'Content-Type': 'application/json' },
+		});
+	}
+
+	return new Response(JSON.stringify(data), {
+		headers: { 'Content-Type': 'application/json' },
+	});
 }
